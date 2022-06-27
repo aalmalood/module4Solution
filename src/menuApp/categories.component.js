cc47f8613842d5d302158abba2bf5b4c9bfa16d7
aalmalood <almalood.91@gmail.com>
2022-06-27 03:48:15 +0300

    Add files via upload

--- src/menuApp/categories.component.js
+++ src/menuApp/categories.component.js
@@ -3,7 +3,7 @@
 
 angular.module('MenuApp')
 .component('categories', {
-  templateUrl: 'src/menuapp/templates/categories.component.template.html',
+  templateUrl: 'src/menuApp/templates/categories.component.template.html',
   bindings: {
     items: '<'
   }
